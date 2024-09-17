0058dbc6b1bf80fe8c6a1f884cd05bdbee6eb83e
Elena Martínez Galán <elena.martinez.galan@hotmail.com>
2024-09-17 21:00:36 +0200

    Solution wargame.py and my_wargame.ipynb

--- wargame.py
+++ wargame.py
@@ -1,25 +1,34 @@
-
-from .vikingsClasses import Soldier, Viking, Saxon, War
+from vikingsClasses import *
 import random
 
 
-soldier_names = ["albert","andres","archie","dani", "david","gerard","german","graham","imanol","laura"]
+soldier_names = ["Mar","Tayna","Gabriel","Elena"]
+n_soldiers = len(soldier_names)
 
+war = War()
 
-#Create 5 Vikings
-for i in range(0,5):
-    if i:
-        War.addViking(Viking(soldier_names[random.randint(0,9)],100,random.randint(0,100)))
+#Create 4 Vikings
+for i in range(0, n_soldiers):
+    war.addViking(Viking(soldier_names[random.randint(0, n_soldiers-1)],100,random.randint(0,100)))
 
-#Create 5 Saxons
-for i in range(0,5):
-    if i:
-        War.addSaxon(Saxon(100,random.randint(0,100)))
-    
+#Create 4 Saxons
+for i in range(0, n_soldiers):
+    war.addSaxon(Saxon(100,random.randint(0,100)))
+
+print("WELCOME TO THE WAR BETWEEN VIKINGS AND SAXONS!\n")
+
 round = 0
-while War.showStatus() == "Vikings and Saxons are still in the thick of battle.":
-    War.vikingAttack()
-    War.saxonAttack()
-    print(f"round: {round} // Viking army: {len(War.vikingArmy)} warriors",f"and Saxon army: {len(War.saxonArmy)} warriors")
-    print(War.showStatus())
+while war.showStatus() == "Vikings and Saxons are still in the thick of battle.":
+    
+    result_attack_viking = war.vikingAttack()
+    print(result_attack_viking)
+
+    result_attack_saxon = war.saxonAttack()
+    print(result_attack_saxon)
+
+    print(f"round: {round} // Viking army: {len(war.vikingArmy)} warriors",f"and Saxon army: {len(war.saxonArmy)} warriors")
+
+    war_status = war.showStatus()
+    print(f"\n{war_status}\n")
+
     round += 1
